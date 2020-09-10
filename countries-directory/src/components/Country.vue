8cd25115b637cca38863d7b40a2154387af0f9fb
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-09-10 10:46:42 +0100

    add axois request and on click for country

--- countries-directory/src/components/Country.vue
+++ countries-directory/src/components/Country.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
-        <div>
-            <p> {{country.name}} </p>
+    <div class="country-box" @click="showCountryDetails">
+      <h4>{{country.name}}</h4>
+      <div v-show="show">
+            <p>Region: {{country.region}}</p>
+            <p>Population: {{country.population}}</p>
+            <p>Captial: {{country.capital}}</p>
         </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Country',
-  props: ["country"]
-
-}
+  name: "Country",
+  props: ["country"],
+  data() {
+      return {
+          show: false
+      }
+  },
+  methods: {
+        showCountryDetails() {
+            this.show= !this.show;
+        }
+    }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.country-box {
+  border: 2px solid;
+  margin: auto;
+  margin-bottom: 5px;
+  width: 60%;
+  
+}
 </style>
