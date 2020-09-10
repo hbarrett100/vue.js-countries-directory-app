1ddde820b027857938a25ac77ceb4f74aa43046f
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-09-10 10:15:53 +0100

    create basic app and components

---
+++ countries-directory/src/components/Country.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+        <div>
+            <p> {{country.name}} </p>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Country',
+  props: ["country"]
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
