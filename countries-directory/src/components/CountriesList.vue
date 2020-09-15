b27648e11761871bb1f8463913be90855e5ba44b
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-09-15 11:48:19 +0100

    clean code

--- countries-directory/src/components/CountriesList.vue
+++ countries-directory/src/components/CountriesList.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-        <div v-for="country in countries" :key="country.name">
-            <Country :country="country" />
-        </div>
+    <div v-for="country in countries" :key="country.name">
+      <Country :country="country" />
+    </div>
   </div>
 </template>
 
 <script>
-import Country from './Country.vue'
+import Country from "./Country.vue";
 
 export default {
-  name: 'CountriesList',
-   components: {
-    Country
+  name: "CountriesList",
+  components: {
+    Country,
   },
-  props: ["countries"]
-
-}
+  props: ["countries"],
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
--- countries-directory/src/components/RangeFilter.vue
+++ countries-directory/src/components/RangeFilter.vue
@@ -2,37 +2,37 @@
   <div>
     <label>{{ label }}</label>
     <VueSimpleRangeSlider
-                    style="width: 250px"
-                    :min="minMaxRange[0]"
-                    :max="minMaxRange[1]"
-                    barColor="#E98074"
-                    activeBarColor="#E98074"
-                    :logarithmic="true"
-                    v-model="range"
-                    @input="$emit('range-changed', range)"
-            />
+      style="width: 250px"
+      :min="minMaxRange[0]"
+      :max="minMaxRange[1]"
+      barColor="#E98074"
+      activeBarColor="#E98074"
+      :logarithmic="true"
+      v-model="range"
+      @input="$emit('range-changed', range)"
+    />
   </div>
 </template>
 <script>
-import VueSimpleRangeSlider from 'vue-simple-range-slider';
-import 'vue-simple-range-slider/dist/vueSimpleRangeSlider.css'
+import VueSimpleRangeSlider from "vue-simple-range-slider";
+import "vue-simple-range-slider/dist/vueSimpleRangeSlider.css";
 export default {
-    // register the component
-    components: { VueSimpleRangeSlider },
-    data() {
-          return {
-               range: []
-          }
-    },
-    props: ["minMaxRange", "label"],
+  // register the component
+  components: { VueSimpleRangeSlider },
+  data() {
+    return {
+      range: [],
+    };
+  },
+  props: ["minMaxRange", "label"],
 
-    //watch for change in minMaxRange prop
-    watch: {
-    minMaxRange: function() {
-      this.range = this.minMaxRange
-    }
-    }
-}
+  //watch for change in minMaxRange prop
+  watch: {
+    minMaxRange: function () {
+      this.range = this.minMaxRange;
+    },
+  },
+};
 </script> 
 
 <style scoped>
@@ -41,6 +41,5 @@ label {
   text-align: left;
   font-size: 1.1em;
 }
-
 </style>
 
