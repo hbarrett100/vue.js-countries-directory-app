76ba44cc5942cb1e4361234782fc84ab48c14af6
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-09-15 12:00:13 +0100

    add server

--- countries-directory/src/components/DropdownFilter.vue
+++ countries-directory/src/components/DropdownFilter.vue
@@ -1,38 +1,52 @@
 <template>
   <div>
     <label class="typo__label">Select Region(s):</label>
-    <multiselect v-model="value" @input="$emit('dropdown-value-changed', value)" :options="options" :multiple="true" :close-on-select="false" :clear-on-select="false" :preserve-search="true" placeholder="-- Select --" :preselect-first="true">
-      <template slot="selection" slot-scope="{ values, search, isOpen }"><span class="multiselect__single" v-if="values.length &amp;&amp; !isOpen">{{ values.length }} options selected</span></template>
+    <multiselect
+      v-model="value"
+      @input="$emit('dropdown-value-changed', value)"
+      :options="options"
+      :multiple="true"
+      :close-on-select="false"
+      :clear-on-select="false"
+      :preserve-search="true"
+      placeholder="-- Select --"
+      :preselect-first="true"
+    >
+      <template slot="selection" slot-scope="{ values, search, isOpen }">
+        <span
+          class="multiselect__single"
+          v-if="values.length &amp;&amp; !isOpen"
+        >{{ values.length }} options selected</span>
+      </template>
     </multiselect>
   </div>
 </template>
 
 <script>
-import Multiselect from 'vue-multiselect'
+import Multiselect from "vue-multiselect";
 
 export default {
-  name: 'DropdownFilter',
+  name: "DropdownFilter",
   components: {
-    Multiselect
+    Multiselect,
   },
   props: ["regions"],
-  data () {
+  data() {
     return {
       value: [],
-      options: []
-    }
+      options: [],
+    };
   },
   watch: {
     // first value in watch fn is new value of prop
-    regions: function(newRegions) {
-    this.options = newRegions;
-    }
-  }
-}
+    regions: function (newRegions) {
+      this.options = newRegions;
+    },
+  },
+};
 </script> 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style scoped>
-
 .multiselect {
   width: 250px;
 }
