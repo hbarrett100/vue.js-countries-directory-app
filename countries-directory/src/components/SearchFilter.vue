fd5d7c5024a578f50b227e697f8e23282c5a620d
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-09-15 11:47:45 +0100

    clean code

--- countries-directory/src/components/SearchFilter.vue
+++ countries-directory/src/components/SearchFilter.vue
@@ -1,30 +1,33 @@
 
 <template>
   <div>
-    <b-form-input v-model="text" placeholder="Search country name.." @keyup="$emit('search-changed', text)"></b-form-input>
+    <b-form-input
+      v-model="text"
+      placeholder="Search country name.."
+      @keyup="$emit('search-changed', text)"
+    ></b-form-input>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'SearchFilter',
-    data() {
-      return {
-        text: ''
-      }
-    }
-
-  }
+export default {
+  name: "SearchFilter",
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
 </script>
 
 <style scoped>
 input {
-  width: 50%
+  width: 50%;
 }
 
 @media only screen and (max-width: 768px) {
   input {
-  width: 100%
+    width: 100%;
   }
 }
 </style>
